ddbdf5765254ba12f4c9412a2d5c28a5a0c093ea
Thy Khuu <62774638+bachthyaglx@users.noreply.github.com>
2025-03-19 04:43:53 +0100

    Delete Part10 directory

--- Part10/frontend/src/components/RepositoryInfo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RepositoryItem from './RepositoryItem';
-
-const RepositoryInfo = ({ repository }) => {
-  return <RepositoryItem item={repository} showGitHubLink={true} />;
-};
-
-export default RepositoryInfo;
